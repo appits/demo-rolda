ef0bd777a64500044ef78b55d43702baf897c557
Keiver Peña <keiverp.robles@gmail.com>
2021-05-01 13:55:10 -0400

    [ADD] rolda: Ajusta libros de venta y compra con columnas adicionales
    Issues:
    Resolve #2

---
+++ rolda/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "Rolda",
+    "version": "13.0.0.0.1",
+    "depends": ['locv_fiscal_book'],
+    "author": "Keiver Peña",
+    "license": "AGPL-3",
+    "website": "https://gitlab.com/keiverobles",
+    "category": "Libros Fiscales-Accounting",
+    "data": [
+        "report/fiscal_purchase_book_report.xml",
+        "report/fiscal_book_report.xml",
+    ],
+    "installable": True,
+}
