d755e8429b1ff34b734377f95e0e23216492d594
Keiver Peña <keiverp.robles@gmail.com>
2021-05-01 14:23:08 -0400

    [IMP] Agrega campos adicionales en Clientes
    Issues:
    Resolve #3

---
+++ rolda/models/res_partner.py
@@ -0,0 +1,11 @@
+from odoo import models, fields
+
+
+class ResPartner(models.Model):
+    _inherit = 'res.partner'
+
+    sale_zone = fields.Text('Zona de venta')
+    segment = fields.Text('Segmento')
+    salesman_code = fields.Char('Cod. Vendedor', size=10)
+    nielsen_code = fields.Integer('Cod. Nielsen')
+    nielsen = fields.Char('Nielsen', size=100)
