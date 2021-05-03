553916b623bb211670da74253215612ce1139aea
Keiver Peña <keiverp.robles@gmail.com>
2021-05-03 19:11:25 -0400

    [ADD] Campo nit en clientes

--- rolda/models/res_partner.py
+++ rolda/models/res_partner.py
@@ -4,6 +4,7 @@ from odoo import models, fields
 class ResPartner(models.Model):
     _inherit = 'res.partner'
 
+    nit = fields.Char('NIT')
     sale_zone = fields.Text('Zona de venta')
     segment = fields.Text('Segmento')
     salesman_code = fields.Char('Cod. Vendedor', size=10)
