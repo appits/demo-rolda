451f67a4a1c7f78970719103fb454e4f57d374e5
Keiver Peña <keiverp.robles@gmail.com>
2021-05-03 19:45:29 -0400

    [WIP] Sección de información del cliente y de la factura

--- rolda/__manifest__.py
+++ rolda/__manifest__.py
@@ -1,15 +1,16 @@
 {
-    "name": "Rolda",
-    "version": "13.0.0.0.1",
-    "depends": ['locv_fiscal_book'],
-    "author": "Keiver Peña",
-    "license": "AGPL-3",
-    "website": "https://gitlab.com/keiverobles",
-    "category": "Libros Fiscales-Accounting",
-    "data": [
-        "views/res_partner_views.xml",
-        "report/fiscal_purchase_book_report.xml",
-        "report/fiscal_book_report.xml",
+    'name': 'Rolda',
+    'version': '13.0.0.0.1',
+    'depends': ['locv_fiscal_book'],
+    'author': 'Keiver Peña',
+    'license': 'AGPL-3',
+    'website': 'https://gitlab.com/keiverobles',
+    'category': 'Libros Fiscales-Accounting',
+    'data': [
+        'views/res_partner_views.xml',
+        'report/fiscal_purchase_book_report.xml',
+        'report/fiscal_book_report.xml',
+        'report/report_invoice.xml',
     ],
-    "installable": True,
+    'installable': True,
 }
