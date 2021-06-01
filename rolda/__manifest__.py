8e12f848dd4f4ea80091f372d89fa696ab0a56e7
Keiver Peña <keiverp.robles@gmail.com>
2021-06-01 17:58:52 -0400

    [REF] Modelo de txt de bancos
    Issues:
    Resolve #8

--- rolda/__manifest__.py
+++ rolda/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Rolda',
-    'version': '13.0.0.0.1',
+    'version': '13.0.0.1.1',
     'depends': ['locv_fiscal_book', 'locv_withholding_iva', 'locv_account_fiscal_requirements'],
     'author': 'Keiver Peña',
     'license': 'AGPL-3',
@@ -12,7 +12,7 @@
         'data/sequence_data.xml',
         'views/assets.xml',
         'views/res_partner_views.xml',
-        'wizard/export_bank_payments_views.xml',
+        'views/export_bank_payments_views.xml',
         'report/rolda_reports.xml',
         'report/fiscal_purchase_book_report.xml',
         'report/fiscal_book_report.xml',
